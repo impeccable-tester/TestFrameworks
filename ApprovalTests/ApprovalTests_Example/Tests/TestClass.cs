2e982e0739b759830cf7b04ebfddce56dbca9aa0
srikanth.nellore <srikanth.nellore@cashconverters.com>
2015-11-27 14:46:20 +0800

    Added more sample tests

--- ApprovalTests/ApprovalTests_Example/Tests/TestClass.cs
+++ ApprovalTests/ApprovalTests_Example/Tests/TestClass.cs
@@ -15,5 +15,33 @@ namespace Tests
             var result = calc.Add(2, 3);
             Approvals.Verify(result);
         }
+
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        public void TestSub()
+        {
+            var calc = new Calculator();
+            var result = calc.Sub(2, 3);
+            Approvals.Verify(result);
+        }
+
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        public void TestMul()
+        {
+            var calc = new Calculator();
+            var result = calc.Mul(2, 3);
+            Approvals.Verify(result);
+        }
+
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        public void TestDiv()
+        {
+            var calc = new Calculator();
+            var result = calc.Div(2, 3);
+            Approvals.Verify(result);
+        }
+
     }
 }
